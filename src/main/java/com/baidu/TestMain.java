cbe081a66ced214361e1f5b4f74ff2f4fe0c6c35
李 <1637467054@qq.com>
2022-05-06 18:11:19 +0800

    测试拉取冲突

--- src/main/java/com/baidu/TestMain.java
+++ src/main/java/com/baidu/TestMain.java
@@ -7,6 +7,6 @@ public class TestMain {
         System.out.println("这是第一次写的代码");
         System.out.println("master冲突合并提交");
         System.out.println("hot-fix修改代码");
-        System.out.println("hot-fix修改代码");
+        System.out.println("这是本地修改代码，为了测试远程代码合并问题");
     }
 }
