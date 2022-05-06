7344934bd1d940d3b8832501efa42d89e617d1b9
李 <1637467054@qq.com>
2022-05-06 17:43:23 +0800

    hot-fix第一次提交

--- src/main/java/com/baidu/TestMain.java
+++ src/main/java/com/baidu/TestMain.java
@@ -4,5 +4,6 @@ public class TestMain {
     public static void main(String[] args) {
         System.out.println("这是第一次写的代码");
         System.out.println("这是第一次写的代码");
+        System.out.println("这是第一次写的代码");
     }
 }
